2be425ab48e710fe2f02ce80d6e436f59e23dc3f
Sunny Patel <sunnypatel4prez@gmail.com>
2024-07-25 09:44:19 -0500

    two sum ii

---
+++ arrays_and_hashing/two_sum_ii.py
@@ -0,0 +1,18 @@
+from typing import List
+
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        
+        l, r = 0, len(numbers) - 1
+
+        while l < r:
+            if numbers[l] + numbers[r] > target:
+                r -= 1
+            elif numbers[l] + numbers[r] < target:
+                l += 1
+            else:
+                return [l + 1, r + 1]
+            
+solution = Solution()
+
+print(solution.twoSum(numbers = [1,2,3,4], target = 3)) # [1, 2]
