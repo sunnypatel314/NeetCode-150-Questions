6418ee3279d6b046f7ecf72269e3a33f90a17d17
Sunny Patel <sunnypatel4prez@gmail.com>
2024-07-25 09:42:02 -0500

    valid palindrome

---
+++ arrays_and_hashing/valid_palindrome.py
@@ -0,0 +1,23 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        
+        l, r = 0, len(s) - 1
+
+        while r >= l:
+            if s[l].isalnum() == False:
+                l += 1
+                continue
+            if s[r].isalnum() == False:
+                r -= 1
+                continue
+
+            if s[l].lower() != s[r].lower():
+                return False
+            r -= 1
+            l += 1
+        return True
+
+solution = Solution()
+
+print(solution.isPalindrome(s = "Was it a car or a cat I saw?")) # True
+print(solution.isPalindrome(s = "tab a cat")) # False
