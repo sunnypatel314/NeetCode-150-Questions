5772873542062a4251015f0d71cb39c152d16b16
Sunny Patel <sunnypatel4prez@gmail.com>
2024-08-13 16:04:47 -0500

    linked list cycle detection

---
+++ linked_list/linked_list_cycle_detection.py
@@ -0,0 +1,17 @@
+from typing import Optional
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        fast, slow = head, head
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
+            if fast == slow:
+                return True
+        return False
